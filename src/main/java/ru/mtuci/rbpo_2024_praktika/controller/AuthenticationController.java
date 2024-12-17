1ef5799f0c369ba239b50c1963b0a094a2b2c798
ZXCBombovoz <megahaker822@gmail.com>
2024-12-17 13:23:21 +0300

    Добавлены новые модули

--- src/main/java/ru/mtuci/rbpo_2024_praktika/controller/AuthenticationController.java
+++ src/main/java/ru/mtuci/rbpo_2024_praktika/controller/AuthenticationController.java
@@ -1,52 +1,55 @@
 package ru.mtuci.rbpo_2024_praktika.controller;
 
+import com.mtuci.ukhanov.configuration.JwtTokenProvider;
+import com.mtuci.ukhanov.models.ApplicationUser;
+import com.mtuci.ukhanov.models.AuthenticationResponse;
+import com.mtuci.ukhanov.repositories.UserRepository;
+import com.mtuci.ukhanov.requests.AuthenticationRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.mtuci.rbpo_2024_praktika.configuration.JwtTokenProvider;
-import ru.mtuci.rbpo_2024_praktika.model.ApplicationUser;
-import ru.mtuci.rbpo_2024_praktika.model.AuthenticationRequest;
-import ru.mtuci.rbpo_2024_praktika.model.AuthenticationResponse;
-import ru.mtuci.rbpo_2024_praktika.repository.UserRepository;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/auth/login")
 @RequiredArgsConstructor
 public class AuthenticationController {
-
     private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider jwtTokenProvider;
 
-    @PostMapping("/login")
+/*
+    Формат ввода: json
+    {
+        "login": "login",
+        "password": "password"
+    }
+*/
+
+    @PostMapping
     public ResponseEntity<?> login(@RequestBody AuthenticationRequest request) {
         try {
-            String email = request.getEmail();
+            String login = request.getLogin();
 
-            ApplicationUser user = userRepository.findByEmail(email)
-                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            Authentication auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(login, request.getPassword())
+            );
 
-            authenticationManager
-                    .authenticate(
-                            new UsernamePasswordAuthenticationToken(
-                                    email, request.getPassword())
-                    );
+            ApplicationUser user = userRepository.findByLogin(login).orElseThrow(() -> new UsernameNotFoundException("user not found"));
 
-            String token = jwtTokenProvider
-                    .createToken(email, user.getRole().getGrantedAuthorities());
+            String token = jwtTokenProvider.createToken(login, user.getRole().getGrantedAuthorities());
 
-            return ResponseEntity.ok(new AuthenticationResponse(email, token));
-        } catch (AuthenticationException ex) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body("Invalid email or password");
+            return ResponseEntity.ok(new AuthenticationResponse(token, login));
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid login or password");
         }
     }
 }
